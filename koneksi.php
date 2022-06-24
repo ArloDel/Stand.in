938f6ef45e6d99b7a499ae4b6fb9a040444764bf
Sendhy Ramadhinata <sendhyrama@gmail.com>
2022-06-24 09:30:02 +0700

    update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,15 @@
-<?php
-$con = mysqli_connect('localhost', 'root','');
-if(!$con)
-{
-    die;
+<?php 
+$host   = "localhost";
+$user   = "root";
+$pass   = "";
+$db     = "stand_in";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die("500 | Internal server error");
+
+}else {
+    
 }
-mysqli_select_db($con,'stand_in');
+
 ?>
