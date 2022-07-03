caaaa22559824676d342f639f2fcc355ed1b1cff
Your Name <20082010095@student.upnjatim.ac.id>
2022-07-03 18:31:15 +0700

    redirect about dan contact page dri home

--- index.php
+++ index.php
@@ -18,8 +18,8 @@ include 'koneksi.php';
         <nav>
             <h4>Standin</h4>
             <ul class="nav-links">
-                <li><a href="">About</a></li>
-                <li><a href="">Contacts</a></li>
+                <li><a href="about_us.php">About</a></li>
+                <li><a href="contact.php">Contacts</a></li>
                 <li class="btn"><a href="signup.php">Sign Up</a></li>
                 <li class="btn"><a href="login.php">Log In</a></li>
             </ul>
