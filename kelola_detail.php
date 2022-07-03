5068bd7137446089af07e546ed9ff60643cb2bcd
Sendhy Ramadhinata <sendhyrama@gmail.com>
2022-07-04 00:51:37 +0700

    fitur hapus stand dari user

--- kelola_detail.php
+++ kelola_detail.php
@@ -1,10 +1,21 @@
 <?php
+session_start();
 include 'koneksi.php';
 
-//memanggil method fungsi
-require 'fungsi.php';
+if (!isset($_SESSION["Login"])) {
+    header('Location:login.php');
+}
 
-//query select table stand
+$id         = "";
+$foto       = "";
+$judul      = "";
+$deskripsi  = "";
+$ukuran     = "";
+$harga      = "";
+$alamat     = "";
+$sukses     = "";
+$error      = "";
+
 $id = $_GET['id'];
 $q = "select * from stand where ID_STAND='$id'";
 $result = mysqli_query($conn, $q);
@@ -27,6 +38,48 @@ $iduser = $data['ID_USER'];
 $q = "select * from user where ID_USER='$iduser'";
 $result = mysqli_query($conn, $q);
 $user = mysqli_fetch_array($result);
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+
+
+if ($op == 'delete') {
+    $id         = $_GET['id'];
+    $sqldelete  = "delete from stand where id_stand = '$id'";
+    $qdelete    = mysqli_query($conn, $sqldelete);
+    if ($qdelete) {
+        $sukses = "Data berhasil dihapus";
+    } else {
+        $error  = "Data gagal dihapus!";
+    }
+}
+
+
+if ($error) {
+?>
+    <div class="alert alert-danger" role="alert" style="text-align:center ;">
+        <?php echo $error ?>
+    </div>
+<?php
+    header("refresh:3;url=kelola.php"); // refresh halaman kelola
+}
+?>
+
+<?php
+if ($sukses) {
+?>
+    <div class="alert alert-success" role="alert" style="text-align:center ;">
+        <?php echo $sukses ?>
+    </div>
+<?php
+    header("refresh:3;url=kelola.php");
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -46,33 +99,39 @@ $user = mysqli_fetch_array($result);
             margin-right: auto;
             width: 100%;
         }
+
         div {
             border-radius: 5px;
             background-color: #f2f2f2;
             padding: 20px;
         }
-        label{
-            margin-left : 20px;
-        }
-        .posisi-gambar{
-            display: flex;
-            float : left;
+
+        label {
+            margin-left: 20px;
         }
 
-        .gambar-stand{
-                height: 350px;
-                width: 400px;
-                background-color: white;
+        .posisi-gambar {
+            display: flex;
+            float: left;
         }
-        .judul{
+
+        .gambar-stand {
+            height: 350px;
+            width: 400px;
+            background-color: white;
+        }
+
+        .judul {
             font-size: 30px;
             display: block;
         }
-        .ket{
-            margin-bottom : 20px;
+
+        .ket {
+            margin-bottom: 20px;
             font-size: 20px;
         }
-        .konten{
+
+        .konten {
             padding: 10px 10px 10px 10px;
             font-size: 17px;
             background-color: white;
@@ -81,15 +140,15 @@ $user = mysqli_fetch_array($result);
             border: 2px solid black;
         }
 
-        td{
-            width : 800px;
-            text-align : left;
-            height : 50px;
+        td {
+            width: 800px;
+            text-align: left;
+            height: 50px;
         }
 
-        .text-area{
-            width : 90%;
-            height : 300px;
+        .text-area {
+            width: 90%;
+            height: 300px;
             padding: 10px 10px 10px 10px;
             font-size: 17px;
             background-color: white;
@@ -98,25 +157,27 @@ $user = mysqli_fetch_array($result);
             border: 2px solid black;
             display: block;
         }
-        .deskripsi{
+
+        .deskripsi {
             font-size: 20px;
-            display:block;
+            display: block;
             display: block;
         }
 
-        .tab-deskripsi{
+        .tab-deskripsi {
             width: 50%;
         }
 
-        .wa{
+        .wa {
             display: flex;
-            float : left;
-            margin-left:10px;
-            width : 150px;
+            float: left;
+            margin-left: 10px;
+            width: 150px;
             height: 70px;
         }
-        .width{
-            width:60%;
+
+        .width {
+            width: 60%;
         }
     </style>
 
@@ -127,20 +188,16 @@ $user = mysqli_fetch_array($result);
         <nav>
             <h4>Standin</h4>
             <ul class="nav-links">
-                <li><a href="">About</a></li>
-                <li><a href="">Contacts</a></li>
-                <li class="btn"><a href="signup.php">Sign Up</a></li>
-                <li class="btn"><a href="login.php">Log In</a></li>
+                <li class="btn" style="background: red;"><a style="color: white;" href="kelola_detail.php?op=delete&id=<?php echo $id ?>" onclick="return confirm('Hapus data stand ?')">Hapus</a></li>
             </ul>
         </nav>
     </div>
 
-    <div>
-        <div class="posisi-gambar">
-                    <img src="img/<?php echo $data['FOTO_STAND'];?>" class="gambar-stand" border="3"></img> 
-        </div>
-    <div class= "main">
-    <label class="judul"><b><?php echo $data['JUDUL']; ?></b> </label>
+    <div class="posisi-gambar">
+        <img src="img/<?php echo $data['FOTO_STAND']; ?>" class="gambar-stand" border="3"></img>
+    </div>
+    <div class="main">
+        <label class="judul"><b><?php echo $data['JUDUL']; ?></b> </label>
         <table>
             <tr>
                 <td>
@@ -152,10 +209,10 @@ $user = mysqli_fetch_array($result);
             </tr>
             <tr>
                 <td>
-                <label class="konten"><?php echo $data['UKURAN'];?> </label>
+                    <label class="konten"><?php echo $data['UKURAN']; ?> </label>
                 </td>
                 <td>
-                    <label class="konten"><?php echo $jenis['NAMA_JENIS'];?></label>
+                    <label class="konten"><?php echo $jenis['NAMA_JENIS']; ?></label>
                 </td>
             </tr>
             <tr>
@@ -167,68 +224,69 @@ $user = mysqli_fetch_array($result);
                 </td>
             </tr>
             <tr>
-                <td>                  
-                    <label class="konten">Rp.<?php echo $data['HARGA_STAND'];?> / Bulan</label>
+                <td>
+                    <label class="konten">Rp.<?php echo $data['HARGA_STAND']; ?> / Bulan</label>
                 </td>
                 <td>
-                    <label class="konten"><?php echo $bahan['NAMA_BAHAN'];?> </label>
+                    <label class="konten"><?php echo $bahan['NAMA_BAHAN']; ?> </label>
                 </td>
             </tr>
-        </table>  
+        </table>
         <table>
             <tr>
                 <td class="tab-deskripsi">
-                <label class="deskripsi"> Deskripsi</label>
+                    <label class="deskripsi"> Deskripsi</label>
                 </td>
                 <td>
-                <label class="ket"> Kota</label>
+                    <label class="ket"> Kota</label>
                 </td>
             </tr>
             <tr>
                 <td class="tab-deskripsi" rowspan="6">
-                <label class="text-area"><?php echo $data['DESKRIPSI'];?></label>
+                    <label class="text-area"><?php echo $data['DESKRIPSI']; ?></label>
                 </td>
                 <td>
-                <label class="konten"><?php echo $data['KOTA'];?> </label>
+                    <label class="konten"><?php echo $data['KOTA']; ?> </label>
                 </td>
             </tr>
             <tr>
                 <td class="width">
-                <label class="ket"> Alamat</label>
+                    <label class="ket"> Alamat</label>
                 </td>
                 <td>
             </tr>
             <tr>
                 <td class="width">
-                <label class="konten"><?php echo $data['ALAMAT'];?> </label>
+                    <label class="konten"><?php echo $data['ALAMAT']; ?> </label>
                 </td>
             </tr>
             <tr>
                 <td class="width">
-                <label class="ket"> No Telp</label>
+                    <label class="ket"> No Telp</label>
                 </td>
             </tr>
             <tr>
                 <td class="width">
-                <label class="konten"><?php echo $user['NO_TELP_USER'];?> </label>
+                    <label class="konten"><?php echo $user['NO_TELP_USER']; ?> </label>
                 </td>
             </tr>
             <tr>
                 <td class="width">
-                <?php 
-                //ganti angka 0 didepan nomor telp menjadi 62
-                $nomor = $user['NO_TELP_USER'];
-                $hp = substr_replace($nomor,'62',0,1);
-                ?>
-                <a href='https://wa.me/<?php echo $hp?>?text=Haloo, saya melihat iklan "<?php echo $data['JUDUL']; ?>" yang anda sewakan di Stand.in, apakah stand tersebut masih tersedia ?' target='_blank'>
-                    <img src='public/img/whatsapp-click.png' class="wa"/> 
-                </a>
+                    <?php
+                    //ganti angka 0 didepan nomor telp menjadi 62
+                    $nomor = $user['NO_TELP_USER'];
+                    $hp = substr_replace($nomor, '62', 0, 1);
+                    ?>
+                    <a href='https://wa.me/<?php echo $hp ?>?text=Haloo, saya melihat iklan "<?php echo $data['JUDUL']; ?>" yang anda sewakan di Stand.in, apakah stand tersebut masih tersedia ?' target='_blank'>
+                        <img src='public/img/whatsapp-click.png' class="wa" />
+                    </a>
                 </td>
             </tr>
 
     </div>
     </div>
-    
+
 
 </body>
+
 </html>
