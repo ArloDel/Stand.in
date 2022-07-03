e3f767f261d924779977f467a7b2ba4affb75b17
BagasT <107825342+bagasarjun@users.noreply.github.com>
2022-07-03 18:43:52 +0700

    ada perubahan biar posisi alamatnya gak nge bug

--- detail_stand.php
+++ detail_stand.php
@@ -46,40 +46,33 @@ $user = mysqli_fetch_array($result);
             margin-right: auto;
             width: 100%;
         }
-
         div {
             border-radius: 5px;
             background-color: #f2f2f2;
             padding: 20px;
         }
-
-        label {
-            margin-left: 20px;
+        label{
+            margin-left : 20px;
         }
-
-        .posisi-gambar {
+        .posisi-gambar{
             display: flex;
-            float: left;
+            float : left;
         }
 
-        .gambar-stand {
-            height: 350px;
-            width: 400px;
-            background-color: white;
+        .gambar-stand{
+                height: 350px;
+                width: 400px;
+                background-color: white;
         }
-
-        .judul {
+        .judul{
             font-size: 30px;
             display: block;
         }
-
-        .ket {
-            margin-bottom: 20px;
+        .ket{
+            margin-bottom : 20px;
             font-size: 20px;
-            font-weight: 600;
         }
-
-        .konten {
+        .konten{
             padding: 10px 10px 10px 10px;
             font-size: 17px;
             background-color: white;
@@ -88,15 +81,15 @@ $user = mysqli_fetch_array($result);
             border: 2px solid black;
         }
 
-        td {
-            width: 800px;
-            text-align: left;
-            height: 50px;
+        td{
+            width : 800px;
+            text-align : left;
+            height : 50px;
         }
 
-        .text-area {
-            width: 90%;
-            height: 300px;
+        .text-area{
+            width : 90%;
+            height : 300px;
             padding: 10px 10px 10px 10px;
             font-size: 17px;
             background-color: white;
@@ -105,24 +98,25 @@ $user = mysqli_fetch_array($result);
             border: 2px solid black;
             display: block;
         }
-
-        .deskripsi {
+        .deskripsi{
             font-size: 20px;
+            display:block;
             display: block;
-            display: block;
-            font-weight: 600;
         }
 
-        .tab-deskripsi {
-            width: 1800px;
+        .tab-deskripsi{
+            width: 50%;
         }
 
-        .wa {
+        .wa{
             display: flex;
-            float: left;
-            margin-left: 10px;
-            width: 150px;
+            float : left;
+            margin-left:10px;
+            width : 150px;
             height: 70px;
+        }
+        .width{
+            width:60%;
         }
     </style>
 
@@ -135,107 +129,106 @@ $user = mysqli_fetch_array($result);
             <ul class="nav-links">
                 <li><a href="">About</a></li>
                 <li><a href="">Contacts</a></li>
-                <li class="btn"><a href="signup.php">Kelola</a></li>
-                <li class="btn"><a href="upload.php">Upload</a></li>
+                <li class="btn"><a href="signup.php">Sign Up</a></li>
+                <li class="btn"><a href="login.php">Log In</a></li>
             </ul>
         </nav>
     </div>
 
     <div>
         <div class="posisi-gambar">
-            <img src="img/<?php echo $data['FOTO_STAND']; ?>" class="gambar-stand" border="3"></img>
+                    <img src="img/<?php echo $data['FOTO_STAND'];?>" class="gambar-stand" border="3"></img> 
         </div>
-        <div class="main">
-            <label class="judul"><b><?php echo $data['JUDUL']; ?></b> </label>
-            <table>
-                <tr>
-                    <td>
-                        <label class="ket"> Ukuran :</label>
-                    </td>
-                    <td>
-                        <label class="ket"> Jenis Stand :</label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="konten"><?php echo $data['UKURAN']; ?> </label>
-                    </td>
-                    <td>
-                        <label class="konten"><?php echo $jenis['NAMA_JENIS']; ?></label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="ket"> Harga :</label>
-                    </td>
-                    <td>
-                        <label class="ket"> Bahan Stand :</label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="konten">Rp.<?php echo $data['HARGA_STAND']; ?> / Bulan</label>
-                    </td>
-                    <td>
-                        <label class="konten"><?php echo $bahan['NAMA_BAHAN']; ?> </label>
-                    </td>
-                </tr>
-            </table>
-            <table>
-                <tr>
-                    <td class="tab-deskripsi">
-                        <label class="deskripsi"> Deskripsi :</label>
-                    </td>
-                    <td>
-                        <label class="ket"> Kota :</label>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="tab-deskripsi" rowspan="6">
-                        <label class="text-area"><?php echo $data['DESKRIPSI']; ?></label>
-                    </td>
-                    <td>
-                        <label class="konten"><?php echo $data['KOTA']; ?> </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="ket"> Alamat :</label>
-                    </td>
-                    <td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="konten"><?php echo $data['ALAMAT']; ?> </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="ket"> No Telp :</label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label class="konten"><?php echo $user['NO_TELP_USER']; ?> </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <?php
-                        //ganti angka 0 didepan nomor telp menjadi 62
-                        $nomor = $user['NO_TELP_USER'];
-                        $hp = substr_replace($nomor, '62', 0, 1);
-                        ?>
-                        <a href='https://wa.me/<?php echo $hp ?>?text=Haloo, saya melihat iklan "<?php echo $data['JUDUL']; ?>" yang anda sewakan di Stand.in, apakah stand tersebut masih tersedia ?' target='_blank'>
-                            <img src='public/img/whatsapp-click.png' class="wa" />
-                        </a>
-                    </td>
-                </tr>
+    <div class= "main">
+    <label class="judul"><b><?php echo $data['JUDUL']; ?></b> </label>
+        <table>
+            <tr>
+                <td>
+                    <label class="ket"> Ukuran</label>
+                </td>
+                <td>
+                    <label class="ket"> Jenis Stand</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                <label class="konten"><?php echo $data['UKURAN'];?> </label>
+                </td>
+                <td>
+                    <label class="konten"><?php echo $jenis['NAMA_JENIS'];?></label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label class="ket"> Harga</label>
+                </td>
+                <td>
+                    <label class="ket"> Bahan Stand</label>
+                </td>
+            </tr>
+            <tr>
+                <td>                  
+                    <label class="konten">Rp.<?php echo $data['HARGA_STAND'];?> / Bulan</label>
+                </td>
+                <td>
+                    <label class="konten"><?php echo $bahan['NAMA_BAHAN'];?> </label>
+                </td>
+            </tr>
+        </table>  
+        <table>
+            <tr>
+                <td class="tab-deskripsi">
+                <label class="deskripsi"> Deskripsi</label>
+                </td>
+                <td>
+                <label class="ket"> Kota</label>
+                </td>
+            </tr>
+            <tr>
+                <td class="tab-deskripsi" rowspan="6">
+                <label class="text-area"><?php echo $data['DESKRIPSI'];?></label>
+                </td>
+                <td>
+                <label class="konten"><?php echo $data['KOTA'];?> </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="width">
+                <label class="ket"> Alamat</label>
+                </td>
+                <td>
+            </tr>
+            <tr>
+                <td class="width">
+                <label class="konten"><?php echo $data['ALAMAT'];?> </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="width">
+                <label class="ket"> No Telp</label>
+                </td>
+            </tr>
+            <tr>
+                <td class="width">
+                <label class="konten"><?php echo $user['NO_TELP_USER'];?> </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="width">
+                <?php 
+                //ganti angka 0 didepan nomor telp menjadi 62
+                $nomor = $user['NO_TELP_USER'];
+                $hp = substr_replace($nomor,'62',0,1);
+                ?>
+                <a href='https://wa.me/<?php echo $hp?>?text=Haloo, saya melihat iklan "<?php echo $data['JUDUL']; ?>" yang anda sewakan di Stand.in, apakah stand tersebut masih tersedia ?' target='_blank'>
+                    <img src='public/img/whatsapp-click.png' class="wa"/> 
+                </a>
+                </td>
+            </tr>
 
-        </div>
     </div>
-
+    </div>
+    
 
 </body>
-
 </html>
